691d540221a22c7f3041839f79ce7f68dd4e32fb
Zolaris <santiago71710@gmail.com>
2026-02-25 09:52:07 -0500

    Actualización final de URL de Script de Google

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,4 +3,4 @@
  * Si necesitas cambiar la URL del script, modifica solo esta constante.
  */
 export const SHEETS_API_URL =
-  "https://script.google.com/macros/s/AKfycbxTA8bNyYUvp12CNt5kAmDza6ij8NpxmjyKAQxqR212_Ap5DEFfOELaJ7QBiB4wTadE/exec";
+  "https://script.google.com/macros/s/AKfycbxzYdoakSx8l0W7IWShmRpbH5u5rkM2PuHGYUeI2E7SzaZnVztq92Ebf8XERvI7gsGA/exec";
